b66b247ab179e87c8095763f071924c010ae1b24
parkjungg <pjk3665@naver.com>
2025-05-30 16:59:17 +0900

    실버 한문제 추가

---
+++ Algorithm2/Silver/Num13335.cs
@@ -0,0 +1,39 @@
+namespace Algorithm2.Silver;
+
+public class Num13335
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = numbers[0];
+        int w = numbers[1];
+        int L = numbers[2];
+        
+        int[] weight = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        
+        Queue<int> queue = new Queue<int>();
+        for(int i = 0; i < w; i++) queue.Enqueue(0);
+
+        int time = 0;
+        int totalWeight = 0;
+        int truckIdx = 0;
+
+        while (truckIdx < n)
+        {
+            time++;
+            totalWeight -= queue.Dequeue();
+
+            if (totalWeight + weight[truckIdx] <= L)
+            {
+                queue.Enqueue(weight[truckIdx]);
+                totalWeight += weight[truckIdx];
+                truckIdx++;
+            }
+            else
+            {
+                queue.Enqueue(0);
+            }
+        }
+        Console.WriteLine(time + w);
+    }
+}
